404618899f6d8a67716ac9b8f7a6cb0a66722c28
rhpessoa <rhpessoa29@gmail.com>
2024-06-14 09:52:30 -0300

    Configurações no projeto para receber os novos componentes criados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+      RouterOutlet,
+      HeaderComponent,
+      FooterComponent,
+      HomeComponent,
+    ],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'portfólio';
 }
